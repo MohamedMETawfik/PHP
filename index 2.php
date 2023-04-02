fc5eb36dc44eb92bcf2639bc532d686948462645
Mohamed Mohsen <mohamed.m.twfik@gamil.com>
2023-04-02 09:06:34 +0200

    task 1,3,4,5 day2 php

--- index 2.php
+++ index 2.php
@@ -13,49 +13,39 @@
 #4
     $array = array(12, 45, 10, 25);
 
-    // Calculate the sum of the array
     $sum = array_sum($array);
 
-    // Calculate the average of the array
     $count = count($array);
     $avg = ($count > 0) ? ($sum / $count) : 0;
 
-    // Output the results
     echo "Sum: " . $sum . "<br>";
     echo "Average: " . $avg . "<br>";
 
-    // Sort the array in reverse order
     arsort($array);
 
-    // Output the sorted array
     echo "Sorted Array (highest to lowest):<br>";
     foreach ($array as $value) {
         echo $value . "<br>";
     }
 
 #5
-    // Define the associative array
     $array = array("Sara" => 31, "John" => 41, "Walaa" => 39, "Ramy" => 40);
 
-    // Sort the array in ascending order by value
     asort($array);
     echo "Ascending order sort by value:<br>";
     print_r($array);
     echo "<br>";
 
-    // Sort the array in ascending order by key
     ksort($array);
     echo "Ascending order sort by key:<br>";
     print_r($array);
     echo "<br>";
 
-    // Sort the array in descending order by value
     arsort($array);
     echo "Descending order sort by value:<br>";
     print_r($array);
     echo "<br>";
 
-    // Sort the array in descending order by key
     krsort($array);
     echo "Descending order sort by key:<br>";
     print_r($array);
